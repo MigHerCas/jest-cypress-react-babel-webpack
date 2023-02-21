813dd4b1e28449a16198eaf0a59c4b0d524bdf05
Miguel Hernanz <migherdcas@gmail.com>
2023-02-21 14:44:10 +0800

    change test environment to jsdom (browser)

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  testEnvironment: 'jest-environment-node' // Browser alternative: 'jest-environment-jsdom',
+  testEnvironment: 'jest-environment-jsdom' // Node alternative: 'jest-environment-node',
 }
