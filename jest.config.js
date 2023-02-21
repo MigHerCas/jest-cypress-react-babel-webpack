ee10c10eb45eb4f117303d2c90b433f22fec8265
Miguel Hernanz <migherdcas@gmail.com>
2023-02-21 15:09:21 +0800

    add explainatory comment

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom', // Node alternative: 'jest-environment-node',
   moduleNameMapper: {
+    // For CSS Modules
     '\\module\\.css$': 'identity-obj-proxy',
+    // For any other CSS file
     '\\.css$': require.resolve('./test/styles-mock.js')
   }
 }
