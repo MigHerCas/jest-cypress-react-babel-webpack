4fc753a4322aa685c8b6a798f2ae131add62678c
Miguel Hernanz <migherdcas@gmail.com>
2023-02-21 15:07:43 +0800

    add new mapper clause for css modules so className is visible in debug mode

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom', // Node alternative: 'jest-environment-node',
   moduleNameMapper: {
+    '\\module\\.css$': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/styles-mock.js')
   }
 }
